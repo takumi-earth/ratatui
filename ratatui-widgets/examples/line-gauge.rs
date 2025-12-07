53ed8b904bff87b26278e81def198c2e34d9389a
Earth Sol <55757024+earth-sol@users.noreply.github.com>
2025-12-07 17:57:47 +0800

    tightening semver level

--- ratatui-widgets/examples/line-gauge.rs
+++ ratatui-widgets/examples/line-gauge.rs
@@ -67,8 +67,8 @@ impl App {
 
     fn handle_events(&mut self) -> Result<()> {
         let timeout = Duration::from_secs_f32(1.0 / 20.0);
-        if event::poll(timeout)? {
-            if let Some(key) = event::read()?.as_key_press_event() {
+        if event::poll(timeout)?
+            && let Some(key) = event::read()?.as_key_press_event() {
                 match key.code {
                     KeyCode::Char(' ') => self.toggle_start(),
                     KeyCode::Char('r') => self.reset(),
@@ -76,7 +76,6 @@ impl App {
                     _ => {}
                 }
             }
-        }
         Ok(())
     }
 
